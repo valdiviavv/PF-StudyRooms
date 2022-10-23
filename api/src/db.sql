95e88ba11d79ea8190cbed06d4837076e725cb39
valdiviavv <valdiviavv@hotmail.com>
2022-10-23 11:28:50 -0400

    [api] The db.sql was updated with table queries.

--- api/src/db.sql
+++ api/src/db.sql
@@ -2,3 +2,7 @@
 create database "StudyRooms";
 create user study_user with encrypted password '1234Study';
 grant all privileges on database "StudyRooms" to study_user;
+
+select * from votesxanswers;
+select * from ratingxanswers;
+select * from answers order by id;
